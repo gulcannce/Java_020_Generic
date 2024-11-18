cbd9b1b3699bd8e04828d07796e8c09c39231804
Gülcan çelik <gulcanncee@gmail.com>
2024-11-18 20:29:56 +0300

    Update AppMain.java

--- src/main/java/com/gulcancelik/v2/AppMain.java
+++ src/main/java/com/gulcancelik/v2/AppMain.java
@@ -5,7 +5,7 @@ import java.util.LinkedList;
 
 public class AppMain {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) { 
 
         Personel obj = new Personel();
       //  obj.ekranaYaz();
